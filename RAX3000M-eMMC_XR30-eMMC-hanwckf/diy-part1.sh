e0a319c5401aeb64b07c184c1c1bf944aaccbec4
ling <1042585959@qq.com>
2025-02-12 16:18:45 +0800

    更新

--- RAX3000M-eMMC_XR30-eMMC-hanwckf/diy-part1.sh
+++ RAX3000M-eMMC_XR30-eMMC-hanwckf/diy-part1.sh
@@ -29,7 +29,7 @@ cp -rf MyConfig/configs/hanwckf/toolchain/* toolchain
 
 #解决elfutils编译错误
 # rm -rf package/libs/elfutils
-# cp -rf MyConfig/configs/hanwckf/package/libs package
+cp -rf MyConfig/configs/hanwckf/package/libs package
 
 # 更新uhttpd版本（防止在GCC版本为13.3.0时报错）
 # rm -rf package/network/services/uhttpd
